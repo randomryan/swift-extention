f1bf460a23a07c11b31e11d56db024ef9c30ccc0
Random Ryan <121579757+randomryan@users.noreply.github.com>
2023-04-21 18:44:43 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,16 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     
-    var data = new FormData(form);
+    var ipAddress = form.elements.ipAddress.value;
+    var location = form.elements.location.value;
+    var latitude = form.elements.latitude.value;
+    var longitude = form.elements.longitude.value;
+    var userAgent = form.elements.userAgent.value;
+    var headers = [];
+    var checkboxes = form.querySelectorAll('input[type=checkbox]:checked');
+    for (var i = 0; i < checkboxes.length; i++) {
+      headers.push(checkboxes[i].value);
+    }
     
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText);
-      }
-    };
-    
-    xhr.open('POST', 'https://example.com/submit.php', true);
-    xhr.send(data);
-  });
-});
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var tab = tabs[0];
+      
+      if (ipAddress) {
+        chrome.webRequest.onBeforeSendHeaders.addListener(
+          function(details) {
+            var requestHeaders = details.requestHeaders;
+            requestHeaders.push({name: 'X-Forwarded-For', value: ipAddress});
+            return {requestHeaders: requestHeaders};
+          },
+          {urls: ["<all_urls>"]},
